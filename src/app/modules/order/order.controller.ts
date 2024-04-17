0585f5e3a75608d07e44fb1286424c6d3fbb9cfe
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-04-17 21:33:55 +0600

    bug solved

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -1,11 +1,11 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response, response } from "express";
 import { OrderServices } from "./order.service";
 
 
 const addNewOrder = async(req:Request,res:Response,next:NextFunction) => {
     try {
         const userId = parseInt(req.params.userId);
-        const result = await OrderServices.addNewOrderIntoDB(userId,req.body)
+        const result = await OrderServices.addNewOrderIntoDB(res,userId,req.body)
         res.json({
             success:true,
             message:'Order Created Successfully',
--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,8 +1,22 @@
+import { Response } from "express";
+import { User } from "../user/user.model";
 import { TOrder } from "./order.interface";
 
 
-const addNewOrderIntoDB = async(userId:number,payload:TOrder) => {
+const addNewOrderIntoDB = async(res:Response,userId:number,payload:TOrder) => {
+    const user = await User.findOne({userId});
+    if(!user){
+        return res.status(404).json({ success: false, message: 'User not found' });
+    }
 
+    if(user.orders && user.orders.length > 0){
+        user.orders.push({payload});
+    } else {
+        user.orders = [{payload}];
+    }
+
+    const result = await user.save();
+    return result;
 }
 
 export const OrderServices = {
